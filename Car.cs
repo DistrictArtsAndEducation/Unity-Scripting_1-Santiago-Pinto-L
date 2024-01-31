7dbe6f3b79e8b21fe58640dbcc81f3f7d5237cf4
AnthonySTP <151577742+AnthonySTP@users.noreply.github.com>
2024-01-31 18:18:06 -0500

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+   [SerializeField] float moveSpeed;
+   float vertical, horizontal;
+   Rigidbody2D myRigidbody2D
+   void Start ()
+   {
+    myRigidbody2D = GetComponent<Rigidbody2D>
+   }
+  
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
